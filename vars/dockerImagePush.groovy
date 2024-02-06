bab0e32f28180f57dfe3604c9fc01b7e0dc5ab31
SANJAY B <75837200+imsanju02k@users.noreply.github.com>
2024-02-06 07:09:26 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTag, String hubUser){
     withCredentials([usernamePassword(
-            credentialsId: "docker",
+            credentialsId: "docker1",
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
