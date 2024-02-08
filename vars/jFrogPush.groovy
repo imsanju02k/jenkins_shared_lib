099bcd1b805c7e7ffe56cddf715ce2d2fa67f75d
SANJAY B <75837200+imsanju02k@users.noreply.github.com>
2024-02-08 15:17:42 +0530

    JFrog Intergration

---
+++ vars/jFrogPush.groovy
@@ -0,0 +1,22 @@
+def call() {
+    withCredentials([usernamePassword(
+            credentialsId: "jfrog",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "jfrog rt config --user=$USER --password=$PASS --interactive=false"
+        sh "jfrog rt docker-login"
+    }
+    // sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"
+}
+
+
+
+// def call(String aws_account_id, String region, String ecr_repoName){
+    
+//     sh """
+//      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+//      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//     """
+// }
